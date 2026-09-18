c1ef8f45404fa2dbf4221e25d0d4576e2b4fd1c5
mkr-21 <mkrumash@gmail.com>
2026-09-18 10:57:51 +0400

    biopython utilized

--- dna_analyzer.py
+++ dna_analyzer.py
@@ -1,15 +1,12 @@
+from Bio.Seq import Seq
+from Bio.SeqUtils import gc_fraction
+
+
+
+
 def gc_content(sequence):
     sequence = sequence.upper()  
-
-    count = 0
-    gc_precentage = 0
-
-    for nucleotide in sequence:
-        if nucleotide == 'G' or nucleotide == 'C':
-            count += 1
-
-    
-    gc_precentage = (count/ len(sequence)) *100
+    gc_precentage = gc_fraction(Seq(sequence)) * 100
     return gc_precentage
 
 
