3efebd53ee3b856567d2f545856e77a7e0473143
mkr-21 <mkrumash@gmail.com>
2026-09-18 09:58:33 +0400

    added the gc-content function

--- dna_analyzer.py
+++ dna_analyzer.py
@@ -1,1 +1,16 @@
+def gc_content(sequence):
+    sequence = sequence.upper()  
+
+    count = 0
+    gc_precentage = 0
+
+    for nucleotide in sequence:
+        if nucleotide == 'G' or nucleotide == 'C':
+            count += 1
+
     
+    gc_precentage = (count/ len(sequence)) *100
+    return gc_precentage
+
+seq = 'ATGCGC'
+print(gc_content(seq))
